491e0e44ea21a9282afcd99eec32b9b67c4e0546
hychoi <hychoi@spacewalk.tech>
2021-08-12 21:53:29 -0400

    Fix NASA POWER API problems

--- spwk_agtech/utils.py
+++ spwk_agtech/utils.py
@@ -5,7 +5,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
-from pcse.db import NASAPowerWeatherDataProvider
+from .nasapower import NASAPowerWeatherDataProvider
 from .const import *
 
 
